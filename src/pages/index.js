70bfb8fa4b66ed8920c6d99ceb580f566fa71be5
Chacha <867571123@qq.com>
2022-02-11 16:01:49 +0800

    feat: update home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 
 import Layout from '../components/layout';
 import CanvasBackground from '../components/canvas';
@@ -6,15 +6,22 @@ import HoverText from '../components/hover-text';
 
 import './styles.scss';
 
-// markup
 const IndexPage = () => {
   return (
     <Layout>
       <section className="w-full h-screen relative bg-slate-900">
-        <Suspense fallback={null}>
-          <CanvasBackground />
-        </Suspense>
+        <CanvasBackground />
         <HoverText />
+        <h2 className="absolute left-4 bottom-4 text-white">
+          Inspired by{' '}
+          <a
+            href="https://tympanus.net/codrops/2022/01/12/pixel-distortion-effect-with-three-js/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Pixel Distortion Effect with Three.js
+          </a>
+        </h2>
       </section>
     </Layout>
   );
