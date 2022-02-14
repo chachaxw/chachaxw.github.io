d24f71af3af1eba441708afaa872a142ef4ada07
Chacha <867571123@qq.com>
2022-02-14 11:14:31 +0800

    feat: update styles

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,32 +1,18 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import Cursor from './cursor';
 import Nav from './nav';
-import Footer from './footer';
 import Seo from './seo';
 
 import './layout.scss';
 
 // markup
 const Layout = ({ children }) => {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(false);
-    }, 2000);
-  }, []);
-
-  if (loading) {
-    return <div className="loading" />;
-  }
-
   return (
     <>
       <Nav />
       <Seo />
       {children}
-      <Footer />
       <Cursor />
     </>
   );
