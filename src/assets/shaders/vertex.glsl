251df9bbafe911034e22064749bd75207a0fd411
Chacha <867571123@qq.com>
2022-02-14 11:13:46 +0800

    feat: add fregment and vertex shaders

---
+++ src/assets/shaders/vertex.glsl
@@ -0,0 +1,10 @@
+uniform float time;
+varying vec2 vUv;
+varying vec3 vPosition;
+varying vec2 pixels;
+float PI=3.141592653589793238;
+
+void main(){
+  vUv=uv;
+  gl_Position=projectionMatrix*modelViewMatrix*vec4(position,1);
+}
