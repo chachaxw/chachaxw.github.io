ea751fe65106bdd6b87b3f34f0f1137256baf082
Chacha <867571123@qq.com>
2022-02-15 16:18:59 +0800

    feat: add nav styles

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import './nav.scss';
+import Logo from './logo';
+
 // Check if window is defined (so if in the browser or in node.js).
 const isBrowser = typeof window !== 'undefined';
 
@@ -9,23 +12,18 @@ const Nav = () => {
 
   return (
     <nav className="fixed w-full flex justify-between items-center text-sm px-8 py-2 z-50">
-      <Link to="/" className="logo"></Link>
-      <div>
-        <Link to="/portfolio" className={`px-4 text-sm ${pathname.includes('/portfolio') ? 'text-white' : ''}`}>
+      <Logo />
+      <div className="link-list">
+        <Link to="/portfolio" className={`link-helike mx-4 text-sm ${pathname.includes('/portfolio') ? 'active' : ''}`}>
           Portfolio
         </Link>
-        <Link to="/careers" className={`px-4 text-sm ${pathname.includes('/careers') ? 'text-white' : ''}`}>
+        <Link to="/careers" className={`link-helike mx-4 text-sm ${pathname.includes('/careers') ? 'active' : ''}`}>
           Careers
         </Link>
-        <Link to="/contact" className={`px-4 text-sm ${pathname.includes('/contact') ? 'text-white' : ''}`}>
+        <Link to="/contact" className={`link-helike mx-4 text-sm ${pathname.includes('/contact') ? 'active' : ''}`}>
           Contact
         </Link>
-        <a
-          href="https://github.com/chachaxw/chachaxw.github.io"
-          target="_blank"
-          className="pl-4 text-sm"
-          rel="noreferrer"
-        >
+        <a href="https://github.com/chachaxw" target="_blank" className="link-helike ml-4 text-sm" rel="noreferrer">
           Github
         </a>
       </div>
