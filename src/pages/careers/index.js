b5d1862eb986c59cec1cba59f9775587b5b00602
Chacha <867571123@qq.com>
2022-02-17 18:45:34 +0800

    feat: update careers page

--- src/pages/careers/index.js
+++ src/pages/careers/index.js
@@ -1,23 +1,25 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import gsap from 'gsap';
 import { Link } from 'gatsby';
 import charming from 'charming';
-import { BiArrowBack } from 'react-icons/bi';
+import { BiArrowBack, BiTransfer } from 'react-icons/bi';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
 import Layout from '../../components/layout';
 import { MathUtils } from '../../utils/utils';
 
 import './styles.scss';
+import myself from '../../assets/images/myself_4.jpg';
 
 gsap.registerPlugin(ScrollTrigger);
 
 const CareersPage = () => {
   const titleRef = useRef();
-  const coverRef = useRef();
-  const imgRef = useRef();
   const textRef = useRef();
   const contentRef = useRef();
+  const sectionRef1 = useRef();
+  const sectionRef2 = useRef();
+  const [visible, setVisible] = useState(false);
   const tl = useRef(gsap.timeline());
 
   useEffect(() => {
@@ -28,14 +30,9 @@ const CareersPage = () => {
       .set([...titleRef.current.children, ...textRef.current.children], {
         opacity: 0,
       })
-      .to(coverRef.current, {
-        delay: 0.25,
-        duration: 0.6,
-        ease: 'power4.out',
-        y: '-100%',
-      })
       .to(titleRef.current.children, {
-        stagger: { each: 0.1 },
+        delay: 0.75,
+        stagger: { each: 0.08 },
         duration: 0.5,
         ease: 'expo.easeIn',
         startAt: {
@@ -58,12 +55,50 @@ const CareersPage = () => {
       });
   }, [tl]);
 
+  const handleClick = () => {
+    const _visible = !visible;
+    setVisible(_visible);
+
+    if (_visible) {
+      tl.current
+        .to(sectionRef1.current, {
+          x: '-100%',
+          ease: 'power1.out',
+        })
+        .to(sectionRef2.current, {
+          x: 0,
+          ease: 'power2.in',
+        });
+    } else {
+      tl.current
+        .to(sectionRef2.current, {
+          x: '100%',
+          ease: 'power2.out',
+        })
+        .to(sectionRef1.current, {
+          x: 0,
+          ease: 'power1.in',
+        });
+    }
+  };
+
+  const handleDragStart = (e) => {
+    console.log('Drag Start', e);
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  const handleDragEnd = (e) => {
+    console.log('Drag End', e);
+  };
+
   return (
     <Layout>
-      <section className="relative w-full h-screen flex bg-secondary text-slate-900">
-        <div className="absolute w-full h-full z-10 bg-secondary" ref={coverRef} />
-        <div className="self-img w-1/2" ref={imgRef} />
-        <div className="w-1/2 h-full p-32 leading-relaxed flex flex-col justify-center" ref={contentRef}>
+      <div className="w-full h-screen fixed retro-blend-darken">
+        <img src={myself} className="absolute right-0" style={{ width: '60%' }} />
+      </div>
+      <section className="absolute top-0 w-full h-screen flex text-slate-900" ref={sectionRef1}>
+        <div className="w-1/2 h-full p-32 leading-relaxed flex flex-col justify-center absolute" ref={contentRef}>
           <Link to="/" className="flex text-slate-900 items-center">
             <BiArrowBack className="text-lg" />
           </Link>
@@ -80,9 +115,9 @@ const CareersPage = () => {
               <span className="font-bold">ThreeJS</span> and <span className="font-bold">Flutter</span> etc.
             </p>
             <p>
-              During this period, I have developed PC-side web applications, mobile-side WebApp, pure{' '}
-              <span className="font-bold">Flutter</span> applications, native and Flutter{' '}
-              <span className="font-bold">hybrid applications</span>, developed{' '}
+              During this period, I have developed some large PC-side and mobile-side web app, pure{' '}
+              <span className="font-bold">Flutter</span> mobile native app,{' '}
+              <span className="font-bold">react-native</span> hybrid app, developed{' '}
               <span className="font-bold">WeChat mini-program</span>, and written{' '}
               <span className="font-bold">NodeJS</span>. Large Front-end project development experience and front-end{' '}
               <span className="font-bold">architecture design</span> experience, over 4 years of front-end{' '}
@@ -92,12 +127,33 @@ const CareersPage = () => {
           </div>
           <span className="block mt-14 w-10 bg-slate-900" style={{ height: 2 }} />
         </div>
-        <h3 className="absolute left-4 bottom-4 text-white text-sm">
-          Inspired by{' '}
-          <a href="https://tympanus.net/codrops/2019/05/16/animated-image-columns/" target="_blank" rel="noreferrer">
-            Animated Image Columns
+        <h3 className="absolute left-4 bottom-4 text-sm">
+          Transition inspired by{' '}
+          <a
+            href="https://tympanus.net/codrops/2017/10/17/dynamic-shape-overlays-with-svg/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Dynamic Shape Overlays with SVG
           </a>
         </h3>
+      </section>
+      <section id="scroll-container" className="scroll-container" ref={sectionRef2}>
+        <div className="scroll-content">
+          <div className="career-title flex flex-col">
+            <span>My</span>
+            <span>Careers</span>
+          </div>
+        </div>
+        <a
+          draggable="true"
+          onClick={handleClick}
+          onDragStart={handleDragStart}
+          onDragEnd={handleDragEnd}
+          className={`enter-button shadow-lg ${visible ? 'visible' : ''}`}
+        >
+          <BiTransfer />
+        </a>
       </section>
     </Layout>
   );
