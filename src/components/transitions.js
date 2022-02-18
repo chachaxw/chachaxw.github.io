32bc04734fd2335a148309917a59dccf00a1f101
Chacha <867571123@qq.com>
2022-02-18 18:49:34 +0800

    feat: add direction and isOpened to Transitions

--- src/components/transitions.js
+++ src/components/transitions.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { ease } from '../utils/ease';
 import './transitions.scss';
 
-const isOpened = false;
 const numPoints = 10;
 const delayPointsArray = [];
 const delayPointsMax = 300;
@@ -15,30 +14,47 @@ for (var i = 0; i < numPoints; i++) {
   delayPointsArray[i] = Math.random() * delayPointsMax;
 }
 
-const Transitions = ({ duration, onUpdate }) => {
+const Transitions = ({ isOpened, direction, duration, onUpdate }) => {
   const svgRef = useRef();
   const [paths, setPaths] = useState([]);
 
-  const updatePath = useCallback((time) => {
-    const points = [];
+  const updatePath = useCallback(
+    (time) => {
+      const points = [];
 
-    for (let i = 0; i < numPoints; i++) {
-      points[i] = (1 - ease.cubicInOut(Math.min(Math.max(time - delayPointsArray[i], 0) / duration, 1))) * 100;
-    }
+      for (let i = 0; i < numPoints; i++) {
+        points[i] = (1 - ease.cubicInOut(Math.min(Math.max(time - delayPointsArray[i], 0) / duration, 1))) * 100;
+      }
 
-    let str = '';
-    str += isOpened ? `M 0 0 V ${points[0]}` : `M 0 ${points[0]}`;
+      let str = '';
 
-    for (let i = 0; i < numPoints - 1; i++) {
-      const p = ((i + 1) / (numPoints - 1)) * 100;
-      const cp = p - ((1 / (numPoints - 1)) * 100) / 2;
-      str += ` C ${cp} ${points[i]} ${cp} ${points[i + 1]} ${p} ${points[i + 1]} `;
-    }
+      if (direction === 'vertical') {
+        str += isOpened ? `M 0 0 V ${points[0]}` : `M 0 ${points[0]}`;
+      } else {
+        str += isOpened ? `M 0 0 H ${points[0]}` : `M ${points[0]} 0`;
+      }
 
-    str += isOpened ? `V 100 H 0` : `V 0 H 0`;
+      for (let i = 0; i < numPoints - 1; i++) {
+        const p = ((i + 1) / (numPoints - 1)) * 100;
+        const cp = p - ((1 / (numPoints - 1)) * 100) / 2;
 
-    return str;
-  });
+        if (direction === 'vertical') {
+          str += ` C ${cp} ${points[i]} ${cp} ${points[i + 1]}  ${p} ${points[i + 1]}`;
+        } else {
+          str += ` C ${points[i]} ${cp} ${points[i + 1]} ${cp} ${points[i + 1]} ${p}`;
+        }
+      }
+
+      if (direction === 'vertical') {
+        str += isOpened ? `V 100 H 0` : `V 0 H 0`;
+      } else {
+        str += isOpened ? `H 100 V 0` : `H 0 V 0`;
+      }
+
+      return str;
+    },
+    [direction, isOpened]
+  );
 
   const render = useCallback(() => {
     if (isOpened) {
@@ -107,12 +123,16 @@ const Transitions = ({ duration, onUpdate }) => {
 };
 
 Transitions.defaultProps = {
+  isOpened: false,
   duration: 900,
+  direction: 'vertical',
 };
 
 Transitions.propTypes = {
+  isOpened: PropTypes.bool,
   duration: PropTypes.number,
   onUpdate: PropTypes.func,
+  direction: PropTypes.oneOf(['vertical', 'horizontal']),
 };
 
 export default Transitions;
