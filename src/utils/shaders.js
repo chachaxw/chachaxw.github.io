1011633fef6c2bb8d07aa070ab2a10c3da4f3a9c
Chacha <867571123@qq.com>
2022-02-18 18:51:24 +0800

    feat: add constants

--- src/utils/shaders.js
+++ src/utils/shaders.js
@@ -8,6 +8,9 @@
  *   2. uniform: 常量，不能被 shader 修改。uniform 变量在 vertex 和 fragment 两只之间的生命方式完全一样，
  *      则它可以在 vertex 和 fragment 共享使用（相当于一个可被共享的全局变量）通常用来传递变换矩阵、光线参数等。
  *   3. varying: varying 变量是 vertex 和 fragment shader 之间做数据传递用的
+ *   4. gl_FragColor    GLSL内建变量 （赋值像素点颜色值）GLSL语言已经提前定义好的变量，有相应的特殊含义。
+ *      内建函数 GLSL提前封装好的函数texture2D(纹理采样器，纹理坐标)，获取对应坐标纹素(读取纹素，读取每一个像素点的颜色值)。
+ *   5. sampler1D, sampler2D, sampler3D 表示不同维度的纹理类型
  */
 
 export const vertex = /* 顶点着色器 */ `
@@ -55,4 +58,31 @@ export const particles = /* 顶点着色器 */ `
     vec4 mvPosition = modelViewMatrix * vec4(position, 1.0);
     gl_PointSize = 1000. * (1.0 / - mvPosition.z);
     gl_Position = projectionMatrix * mvPosition;
+  }`;
+
+export const vertex1 = /* 顶点着色器 */ `
+  uniform float scale;
+  uniform float factor;
+  varying vec2 vUv;
+  void main() {
+    vec3 pos = position;
+    pos.x = pos.x + ((sin(uv.y * 3.1415926535897932384626433832795) * factor * 2.0) * 0.125);
+    vUv = uv;
+    gl_Position = projectionMatrix * modelViewMatrix * vec4(pos,1.);
+  }
+`;
+
+export const fragment1 = /* 片段着色器 */ `
+  uniform sampler2D tex;
+  uniform float factor;
+  uniform float scale;
+  varying vec2 vUv;
+  void main() {
+    float angle = 0.0;
+    vec2 p = (vUv - vec2(0.5, 0.5)) * (1.0 - scale) + vec2(0.5, 0.5);
+    vec2 offset = factor / 50.0 * vec2(cos(angle), sin(angle));
+    vec4 cr = texture2D(tex, p + offset);
+    vec4 cga = texture2D(tex, p);
+    vec4 cb = texture2D(tex, p - offset);
+    gl_FragColor = vec4(cr.r, cga.g, cb.b, cga.a);
   }`;
