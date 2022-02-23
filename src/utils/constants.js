359111072da5709614e0f47f5d71aea8e5069db5
Chacha <867571123@qq.com>
2022-02-23 18:52:24 +0800

    feat: add constants

---
+++ src/utils/constants.js
@@ -0,0 +1,46 @@
+import img1 from '../assets/images/gallery/gallery_1.png';
+import img2 from '../assets/images/gallery/gallery_2.png';
+
+export const galleryList = [
+  {
+    title: 'Front-end Tech Expert',
+    time: 'Future',
+    tags: ['#More...'],
+    description: 'Have a lofty passion and interest in code and design, currently focusing on front-end technology',
+  },
+  {
+    title: 'Front-end Team Leader',
+    time: '2019 - Now',
+    tags: ['#React', '#Flutter', '#iOS', '#Android', '#ThreeJS'],
+    description:
+      "As the leader of a large front-end team, responsible for integrated business management platform, the company's official website, mini-program and native app project management. Developed four Flutter apps, four management systems, and four mini-program during this time.",
+  },
+  {
+    title: 'Freelance Front-end Developer',
+    time: '2017-19',
+    tags: ['#Javascript', '#Typescript', '#React', '#Redux', '#Web3'],
+    description:
+      'As the team leader of 6 front-end development teams from all over the world, responsible for front-end project tech stack research and development, web app performance optimization, and project architecture design and tech stack selection, web app continuous integration and deployment of large-scale US municipal projects.',
+  },
+  {
+    title: 'Front-end Developer',
+    time: '2016-17',
+    tags: ['#React', '#Vue', '#Angular', '#NodeJS'],
+    description:
+      "As a core member of the company's technical team, responsible for front-end development tech stack selection, and front-end tech architecture and refactoring, implementation of front-end continuous interaction and deployment, and develop interactive responsive front-end page.",
+  },
+  {
+    title: 'Front-end Intern',
+    time: '2015-16',
+    tags: ['#React', '#NodeJS', '#Webpack'],
+    description:
+      "Responsible for the company's webapp visual design and project development. And actually conduct user research to improve the user experience. Using the React framework, ES6 and Commonjs to develop the front-end page components, and using Ajax to communicate with the back-end.",
+  },
+  {
+    title: 'Front-end Novice',
+    time: '2013-15',
+    tags: ['#HTML', '#CSS', '#Javascript'],
+    description:
+      'Self-taught front-end development in school, and learning HTML5, CSS3 and Javascript, learning Photoshop and Illustrator to design app and web. Developed a website in collaboration with students in Canada remotely. ',
+  },
+];
